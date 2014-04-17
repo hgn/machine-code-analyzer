3e82502286891e5bdcdb556b1301d91ec6d43624
Hagen Paul Pfeifer <hagen@jauu.net>
2014-04-17 22:22:51 +0200

    parse objdump output
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- instruction-layout-analyzer.py
+++ instruction-layout-analyzer.py
@@ -4,24 +4,51 @@
 import sys
 import argparse
 import logging
+import subprocess
+import re
 
 
 class InstructionLayoutAnalyzer:
-
 
     def __init__(self, filename):
         self.filename = filename
         self.log = logging.info
 
-    def parse_line(self, line):
-        pass
+    def divide_parts(self, line):
+        # 404e52:   e8 31 c2 ff ff          callq  401088 <_init>
+        ret = dict()
+        opcode = ""
+        match = re.search(r' ([\da-f]+):\s+((?:[0-9a-f]{2} )+)', line)
+        if match:
+            opcode += match.group(2).strip() + " "
+            print("%s%s%s" % (Colors.WARNING, opcode, Colors.ENDC))
+        
 
-    def open_parse(self):
-        pass
+    def parse_line(self, line):
+        sys.stdout.write("%s\n" % (line))
+        self.divide_parts(line)
+
+
+    def open_parse(self, filename):
+        cmd = 'objdump -S %s' % (filename)
+        self.log('execute: \"%s\"' % (cmd))
+        p = subprocess.Popen(cmd.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in p.stdout.readlines():
+            self.parse_line(line.decode("utf-8").rstrip())
+
 
     def run(self):
         self.log("Filename to analyze: %s" % args.argument)
-        pass
+        self.open_parse(args.argument)
+
+
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
 
 
 def main(args, loglevel):
