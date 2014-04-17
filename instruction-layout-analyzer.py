008c28f982373d3e6ea2c6226d80277b6f8a5f94
Hagen Paul Pfeifer <hagen@jauu.net>
2014-04-17 23:35:57 +0200

    asm parsing nearly complete
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- instruction-layout-analyzer.py
+++ instruction-layout-analyzer.py
@@ -17,15 +17,51 @@ class InstructionLayoutAnalyzer:
     def divide_parts(self, line):
         # 404e52:   e8 31 c2 ff ff          callq  401088 <_init>
         ret = dict()
-        opcode = ""
-        match = re.search(r' ([\da-f]+):\s+((?:[0-9a-f]{2} )+)', line)
-        if match:
-            opcode += match.group(2).strip() + " "
-            print("%s%s%s" % (Colors.WARNING, opcode, Colors.ENDC))
+        match = re.search(r'([\da-f]+):\s+((?:[0-9a-f]{2} )+)\s+(.*)', line)
+        if not match:
+            return None
+
+        addr   = match.group(1).strip()
+        opcode = match.group(2).strip()
+        instr  = match.group(3).strip()
+
+        # now unfold asm, following online tools
+        # can be used to test regular expressions:
+        # http://www.regexr.com/ or http://rubular.com/
+        # movsd  0x1e4e(%rip),%xmm1        # 406c28 <symtab.5300+0x48>
+        m1 = re.search(r'(\w+)\s+(\S+),(\S+)\s+#(.+)', instr)
+        # mov    0x18(%rsp),%r12
+        m2 = re.search(r'(\w+)\s+(\S+),(\S+)', instr)
+        # jne    404e60 <__libc_csu_init+0x50>
+        m3 = re.search(r'(\w+)\s+(\S+)\s+(<\S+>)', instr)
+        # jmp    404b20 
+        m4 = re.search(r'(\w+)\s+(\S+)', instr)
+        # jmp
+        m5 = re.search(r'(\w+)', instr)
+
+        print("%s%s: %s%s\t\t%s%s%s" % (Colors.WARNING, addr, Colors.OKGREEN, opcode, Colors.FAIL, instr, Colors.ENDC))
+
+        if m1:
+            self.log("1 - movsd  0x1e4e(%rip),%xmm1        # 406c28 <symtab.5300+0x48>\n")
+        elif m2:
+            self.log("2 - mov    0x18(%rsp),%r12\n")
+        elif m3:
+            self.log("3 - jne    404e60 <__libc_csu_init+0x50>\n")
+        elif m4:
+            self.log("4 jmp 404b20\n")
+        elif m5:
+            self.log("5 jmp\n")
+        else:
+            self.log("Something wrong here; %s" % (line))
+            sys.exit(1)
+
+        mnemonic = match.group(1).strip()
+
+        
         
 
     def parse_line(self, line):
-        sys.stdout.write("%s\n" % (line))
+        #sys.stdout.write("%s\n" % (line))
         self.divide_parts(line)
 
 
@@ -34,7 +70,7 @@ class InstructionLayoutAnalyzer:
         self.log('execute: \"%s\"' % (cmd))
         p = subprocess.Popen(cmd.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for line in p.stdout.readlines():
-            self.parse_line(line.decode("utf-8").rstrip())
+            self.parse_line(line.decode("utf-8").strip())
 
 
     def run(self):
