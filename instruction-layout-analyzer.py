dd533a5376b9448830ccb344548b7b6354382001
Hagen Paul Pfeifer <hagen@jauu.net>
2014-04-21 16:12:39 +0200

    fix output handling
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- instruction-layout-analyzer.py
+++ instruction-layout-analyzer.py
@@ -33,7 +33,6 @@ class InstructionLayoutAnalyzer:
 
     def __init__(self, filename):
         self.filename = filename
-        self.log = logging.info
 
     def divide_parts(self, line):
         # 404e52:   e8 31 c2 ff ff          callq  401088 <_init>
@@ -60,20 +59,21 @@ class InstructionLayoutAnalyzer:
         # jmp
         m5 = re.search(r'(\w+)', instr)
 
-        print("%s%s: %s%s\t\t%s%s%s" % (Colors.WARNING, addr, Colors.OKGREEN, opcode, Colors.FAIL, instr, Colors.ENDC))
+        log("%s%s: %s%s\t\t%s%s%s" %
+            (Colors.WARNING, addr, Colors.OKGREEN, opcode, Colors.FAIL, instr, Colors.ENDC))
 
         if m1:
-            self.log("1 - movsd  0x1e4e(%rip),%xmm1        # 406c28 <symtab.5300+0x48>\n")
+            log("1 - movsd  0x1e4e(%rip),%xmm1        # 406c28 <symtab.5300+0x48>")
         elif m2:
-            self.log("2 - mov    0x18(%rsp),%r12\n")
+            log("2 - mov    0x18(%rsp),%r12")
         elif m3:
-            self.log("3 - jne    404e60 <__libc_csu_init+0x50>\n")
+            log("3 - jne    404e60 <__libc_csu_init+0x50>")
         elif m4:
-            self.log("4 jmp 404b20\n")
+            log("4 jmp 404b20")
         elif m5:
-            self.log("5 jmp\n")
+            log("5 jmp")
         else:
-            self.log("Something wrong here; %s" % (line))
+            log("Something wrong here; %s" % (line))
             sys.exit(1)
 
         mnemonic = match.group(1).strip()
@@ -88,31 +88,40 @@ class InstructionLayoutAnalyzer:
 
     def open_parse(self, filename):
         cmd = 'objdump -S %s' % (filename)
-        self.log('execute: \"%s\"' % (cmd))
+        log('pass one: \"%s\"' % (cmd))
         p = subprocess.Popen(cmd.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for line in p.stdout.readlines():
             self.parse_line(line.decode("utf-8").strip())
 
 
     def run(self):
-        self.log("Filename to analyze: %s" % args.argument)
+        log("Filename to analyze: %s" % args.argument)
         self.open_parse(args.argument)
 
 
 class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
+    HEADER  = '\033[95m'
+    OKBLUE  = '\033[94m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    FAIL    = '\033[91m'
+    ENDC    = '\033[0m'
 
 
-def main(args, loglevel):
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
-
+def main(args):
     ila = InstructionLayoutAnalyzer(args.argument)
     ila.run()
+
+
+log_enabled = 0
+
+def log(string):
+    if not log_enabled:
+        return
+    sys.stderr.write("%s\n" % (string))
+
+def out(string):
+    sys.stdout.write("%s\n" % (string))
 
  
 if __name__ == '__main__':
@@ -127,12 +136,18 @@ if __name__ == '__main__':
             help="increase output verbosity",
             action="store_true")
     args = parser.parse_args()
+
+    if args.verbose:
+        log_enabled = True
+
+    if not sys.stdout.isatty():
+        # reset colors
+        Colors.HEADER = ''
+        Colors.OKBLUE = ''
+        Colors.OKGREEN = ''
+        Colors.WARNING = ''
+        Colors.FAIL = ''
+        Colors.ENDC = ''
   
   # Setup logging
-    if args.verbose:
-        loglevel = logging.DEBUG
-    else:
-        loglevel = logging.INFO
-        
-        
-    main(args, loglevel)
+    main(args)
