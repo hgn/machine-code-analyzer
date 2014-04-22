11c44871e387110603c4a6dd820d6851b1ad4071
Hagen Paul Pfeifer <hagen@jauu.net>
2014-04-22 21:17:12 +0200

    more code, more help
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- README.md
+++ README.md
@@ -1,11 +1,91 @@
-# instruction-layout-analyzer #
+# Instruction and Opcode Analyzer #
 
-A two pass code analyzer
+A two pass code analyzer for x86 and x86\_64, requires debug symbols for full
+analyze capabilities.
 
-## Usage ##
+## Modules ##
+
+### Function Local Branch and Jump Analyzer ###
+
+Analyze local jump instructions. "Local" refers to jumps within the function
+space - function calls and long jumps are not analyzed by this module.
+
+Following data calcalated:
+
+- Jump analyzer
+..- Number of jumps
+..- Average jump distance in bytes
+..- Minimal jump distance in bytes
+..- Maximal jump distance in bytes
+..- Histogram of jumps
+- Forward jump analyzer
+..- Number of jumps
+..- Average jump distance in bytes
+..- Minimal jump distance in bytes
+..- Maximal jump distance in bytes
+..- Histogram of jumps
+- Number of backward jumps per function
+..- Number of jumps
+..- Average jump distance in bytes
+..- Minimal jump distance in bytes
+..- Maximal jump distance in bytes
+..- Histogram of jumps
+
+
+
+#### Usage ####
+
+```
+# instruction-layout-analyzer --function-branch-jump <binary>
+```
+
+### Function Anatomy Analyzer ###
+
+Started as an internal module for other modules this module provides
+information about the function start and end addresses, the size of a function
+and the number of functions.
+
+
+#### Usage ####
+
+```
+# instruction-layout-analyzer --function-anatomy <binary>
+```
+
+
+# FAQ #
+
+## Sometimes instruction analyzer and readelf differs from the code size - why? ##
+
+Consider the following end of an function:
 
 
 ```
-instruction-layout-analyzer.py /path/to/binary
+405021:       5d                      pop    %rbp
+405022:       c3                      retq
+405023:       66 2e 0f 1f 84 00 00    nopw   %cs:0x0(%rax,%rax,1)
+40502a:       00 00 00
+40502d:       0f 1f 00                nopl   (%rax)
 ```
 
+What you see here are alignmend padding in form of NOP instructions (13 byte).
+The code actual function end at retq instruction. The subsequent code is now
+aligned at 0x405030, better for cachelines, etc.  The question now is: should
+the instruction analayzer account the padding instructions or not? Feel free
+and verify code with
+
+```
+readelf -s <binary>
+```
+
+NOPs are mostly generated and added by GAS (GNU assembler if using GCC). There
+are other ways as well to decode NOPs:  xchg %ax,%ax (two byte), leal
+0(%esi),%esi (three byte).
+
+Last note: the assembler should always pad with as less instruction as possible
+to relax CPU prefetcher and subsequent CPU logic.
+
+
+# JSON to Chart.js Converter #
+
+To generate charts a helper script comes bundled with instruction-layout-analyzer.
--- instruction-layout-analyzer.py
+++ instruction-layout-analyzer.py
@@ -24,6 +24,13 @@ class InstructionCategory:
     def guess(instructon):
         return InstructionCategory.UNKNOWN
 
+class Context:
+
+    def __init__(self):
+        self.function_name = None
+        self.function_start_address  = None
+        self.section_name  = None
+
 
 class BinaryAtom:
 
@@ -68,38 +75,127 @@ class BinaryAtom:
             raise Exception("unknown code")
 
     def print(self):
-        msg("%s\n" % (self.line)) 
-        msg("MNEMONIC: %s  SRC:%s  DST:%s [OPCODE: %s,  LEN:%d]\n" %
+        dbg("%s\n" % (self.line))
+        dbg("MNEMONIC: %s  SRC:%s  DST:%s [OPCODE: %s,  LEN:%d]\n" %
             (self.mnemonic, self.src, self.dst,  self.opcode_str, self.opcode_len)) 
+
+
+class FunctionAnatomyAnalyzer:
+
+    def __init__(self):
+        self.db = dict()
+
+    def pass1(self, context, atom):
+        if not context.function_name in self.db:
+            self.db[context.function_name] = dict()
+            self.db[context.function_name]['start'] = \
+                    context.function_start_address
+            self.db[context.function_name]['end'] = \
+                    context.function_start_address + atom.opcode_len
+            self.db[context.function_name]['size'] = \
+                    self.db[context.function_name]['end'] - self.db[context.function_name]['start']
+            return
+
+        self.db[context.function_name]['end'] += atom.opcode_len
+        self.db[context.function_name]['size'] = \
+                self.db[context.function_name]['end'] - self.db[context.function_name]['start']
         
 
+    def pass2(self, context, atom):
+        pass
 
-class JumpAnalyser:
+    def show(self, json=False):
+        if json:
+            self.show_json()
+        else:
+            self.show_human()
+
+    def show_human(self):
+        msg("Functions Anatomy:\n")
+        for key in sorted(self.db.items(), key=lambda item: item[1]['size'], reverse=True):
+            msg("%30.30s  [size: %6d byte, start:0x%x, end:0x%x]\n"
+                % (key[0], key[1]['size'], key[1]['start'], key[1]['end']))
+
+    def show_json(self):
+        pass
+
+
+class FunctionBranchJumpAnalyser:
 
     def __init__(self):
         pass
 
-    def pass1(self, blob):
+    def pass1(self, context, atom):
         pass
 
-    def pass2(self, blob):
+    def pass2(self, context, atom):
         pass
 
+    def show(self, json=False):
+        if json:
+            self.show_json()
+        else:
+            self.show_human()
 
-    def final(self):
+    def show_human(self):
+        msg("Functions Branches and Jumps:\n")
+
+    def show_json(self):
         pass
 
 
 class InstructionLayoutAnalyzer:
 
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, args):
+        self.args = args
+        self.filename = args.argument
+        self.init_modules()
 
-    def divide_parts(self, line):
+
+    def init_modules(self):
+        self.module = dict()
+        self.module['FunctionBranchJumpAnalyser'] = FunctionBranchJumpAnalyser()
+        self.module['FunctionAnatomyAnalyzer'] = FunctionAnatomyAnalyzer()
+
+
+    def try_parse_update_context(self, line, context):
+        if line == "":
+            # empty line, do nothing
+            return
+        # ./ipproof-client:     file format
+        match = re.search(r'\s*' + self.filename + ':\s+file format', line)
+        if match:
+            return
+
+        # 0000000000401088 <_init>:
+        match = re.search(r'^([\da-f]+)\s+<(\S+)>', line)
+        if match:
+            context.function_start_address = int(match.group(1).strip(), 16)
+            context.function_name = match.group(2).strip()
+            dbg("Function name: %s, function start address: 0x%x\n" %
+                (context.function_name, context.function_start_address))
+            return
+        # Disassembly of section .plt:
+        match = re.search(r'Disassembly of section\s+(\S+):', line)
+        if match:
+            context.section_name = match.group(1).strip()
+            dbg("Section name: %s\n" % (context.section_name))
+            return
+
+        # unknown lines are probably annotated source code
+        # lines from DWARF info - just skip it
+        #log("Unknown line: \"%s\"\n" % (line))
+
+
+
+    def parse_line(self, line, context):
         # 404e52:   e8 31 c2 ff ff          callq  401088 <_init>
         ret = dict()
         match = re.search(r'([\da-f]+):\s+((?:[0-9a-f]{2} )+)\s+(.*)', line)
         if not match:
+            # no instruction, but maybe function information to
+            # update context data
+            self.try_parse_update_context(line, context)
             return None
 
         addr   = match.group(1).strip()
@@ -169,35 +265,39 @@ class InstructionLayoutAnalyzer:
         return None
 
 
-    def parse_line(self, line):
-        dbg("%s\n" % (line))
-        return self.divide_parts(line)
-
-
-    def open_parse(self, filename):
+    def process(self, filename):
         cmd = 'objdump -S %s' % (filename)
+        context = Context()
 
-        log('pass one: \"%s\"' % (cmd))
+        log('pass one: \"%s\"\n' % (cmd))
         p = subprocess.Popen(cmd.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for line in p.stdout.readlines():
-            atom = self.parse_line(line.decode("utf-8").strip())
+            atom = self.parse_line(line.decode("utf-8").strip(), context)
             if atom is None:
                 continue
+            # Function Anatomy Analyzer is always processed, because
+            # the analyzer serves as a ground work for other analyzes
+            self.module['FunctionAnatomyAnalyzer'].pass1(context, atom)
+            if self.args.function_branch_jump:
+                self.module['FunctionBranchJumpAnalyser'].pass1(context, atom)
             atom.print()
 
         return
 
-        log('pass two: \"%s\"' % (cmd))
-        p = subprocess.Popen(cmd.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        for line in p.stdout.readlines():
-            atom = self.parse_line(line.decode("utf-8").strip())
+    def show(self):
+        if self.args.function_anatomy:
+            self.module['FunctionAnatomyAnalyzer'].show(json=args.json)
+        if self.args.function_branch_jump:
+            self.module['FunctionBranchJumpAnalyser'].show(json=args.json)
+
 
 
     def run(self):
-        msg("Instruction Layout Analyzer - 2014\n")
+        msg("Instruction and Opcode Analyzer - 2014\n")
         msg("URL: https://github.com/hgn/instruction-layout-analyzer\n")
-        msg("Filename to analyze: %s\n" % args.argument)
-        self.open_parse(args.argument)
+        msg("Binary to analyze: %s\n" % args.argument)
+        self.process(args.argument)
+        self.show()
 
 
 class Colors:
@@ -210,7 +310,7 @@ class Colors:
 
 
 def main(args):
-    ila = InstructionLayoutAnalyzer(args.argument)
+    ila = InstructionLayoutAnalyzer(args)
     ila.run()
 
 
@@ -242,6 +342,15 @@ if __name__ == '__main__':
     parser.add_argument("-v", "--verbose",
             help="increase output verbosity",
             action="store_true")
+    parser.add_argument("-j", "--json",
+            help="print results JSON encoded",
+            action="store_true")
+    parser.add_argument("--function-branch-jump",
+            help="function local branch and jump analyzer",
+            action="store_true")
+    parser.add_argument("--function-anatomy",
+            help="function anatomy analyzer",
+            action="store_true")
     parser.add_argument("-d", "--debug",
             help="debug",
             action="store_true")
@@ -250,7 +359,7 @@ if __name__ == '__main__':
     if args.verbose:
         log_enabled = True
     if args.debug:
-        log_debug = True
+        dbg_enabled = True
 
     if not sys.stdout.isatty():
         # reset colors
