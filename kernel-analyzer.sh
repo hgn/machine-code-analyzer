c35b01c97777c34b8a080a18bfcf1d77c54fe750
Hagen Paul Pfeifer <hagen@jauu.net>
2014-11-30 21:56:32 +0100

    point to new kernel
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- kernel-analyzer.sh
+++ kernel-analyzer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-time ./machine-code-analyzer.py instruction-analyzer /home/pfeifer/src/code/01-own/linux-dev/build/arch/x86/boot/compressed/vmlinux.bin
+time ./machine-code-analyzer.py instruction-analyzer vmlinux
