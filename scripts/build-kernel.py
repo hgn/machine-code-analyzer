ccd936bada25ad6954c425cac3f0a3e78128ad91
Hagen Paul Pfeifer <hagen@jauu.net>
2014-12-28 19:10:19 +0100

    do not generate kernel with fentry function prologue
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- scripts/build-kernel.py
+++ scripts/build-kernel.py
@@ -7,6 +7,9 @@ import readline
 import subprocess
 import multiprocessing
 import datetime
+import tempfile
+import shutil
+import re
 
 KERNEL_VERSION = "v3.17"
 
@@ -23,6 +26,36 @@ def sizeof_fmt(num, suffix='B'):
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
+
+def disable_config(config_file_path):
+    options = [
+            "CONFIG_FTRACE",
+            "CONFIG_TRACING_SUPPORT",
+            "CONFIG_HAVE_DYNAMIC_FTRACE",
+            "HAVE_FTRACE_MCOUNT_RECORD"
+            ]
+    #Create temp file
+    fh, abs_path = tempfile.mkstemp()
+    new_file = open(abs_path,'w')
+    old_file = open(config_file_path)
+    for line in old_file:
+        found = False
+        for option in options:
+            if re.match("%s=" % (option), line):
+                print("Disabling kernel config %s" % (option))
+                new_file.write("# %s is not set\n" % (option))
+                found = True
+        if not found:
+            new_file.write(line)
+    #close temp file
+    new_file.close()
+    os.close(fh)
+    old_file.close()
+    #Remove original file
+    os.remove(config_file_path)
+    #Move new file
+    shutil.move(abs_path, config_file_path)
+
 
 filename = "%s/vmlinux" % (KERNEL_BUILD_DIR)
 if os.path.isfile(filename):
@@ -60,6 +93,14 @@ os.chdir(KERNEL_SRC_DIR)
 cmd = "make O=%s allyesconfig" % (KERNEL_BUILD_DIR)
 print("Exectute: \"%s\"" % (cmd))
 os.system(cmd)
+
+disable_config("%s/.config" % (KERNEL_BUILD_DIR))
+
+cmd = "make O=%s oldconfig" % (KERNEL_BUILD_DIR)
+print("Exectute: \"%s\"" % (cmd))
+os.system(cmd)
+sys.exit(0)
+
 
 build_start = datetime.datetime.now()
 # EXTRA_CFLAGS=\"-g -fverbose-asm\"
